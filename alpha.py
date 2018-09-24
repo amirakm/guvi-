957546010506d6ac6deb2492ee09081ce5f7f7f9
amirakm <42800800+amirakm@users.noreply.github.com>
2018-09-24 14:00:28 +0530

    Update alpha.py

--- alpha.py
+++ alpha.py
@@ -1,5 +1,5 @@
 char=raw_input("Enter a character:")
 if((char>='a' and char<= 'z')or(char>='A' and char<='Z')):
-    print("the char is an Alphabet")
+    print("Alphabet")
 else:
-    print("the char is not an Alphabet")
+    print("no")
