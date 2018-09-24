61425ad9a44fbc5c9388a32d11db505cc81258b7
amirakm <42800800+amirakm@users.noreply.github.com>
2018-09-24 11:27:18 +0530

    Update evenodd.py

--- evenodd.py
+++ evenodd.py
@@ -1,5 +1,5 @@
 input_num = int(input('Enter any number: '))
-if input_num % 2 == 0:
-    print(input_num, "is EVEN")
+if input_num %2==0:
+    print("num is even")
 else:
-    print(input_num, "is ODD")
+    print("num is odd")
