ec75223371cc112522696d2a9e9ea8cdee70ccef
amirakm <42800800+amirakm@users.noreply.github.com>
2018-09-25 10:43:10 +0530

    Create power.py

---
+++ power.py
@@ -0,0 +1,9 @@
+#program for power of a number
+def power(base,exp):
+    if(exp==1):
+        return(base)
+    if(exp!=1):
+        return(base*power(base,exp-1))
+base=int(input(""))
+exp=int(input(""))
+print(power(base,exp))
