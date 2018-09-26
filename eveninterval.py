7b54d82ab4e343ad6495e2976df468308472e038
amirakm <42800800+amirakm@users.noreply.github.com>
2018-09-26 10:42:26 +0530

    Update eveninterval.py

--- eveninterval.py
+++ eveninterval.py
@@ -1,4 +1,4 @@
-#program to print even numbers in given intervals
+#program to print even numbers
 lower_limit, upper_limit=map(int,raw_input().split())
 for i in range(lower_limit,upper_limit+1):
   if(i%2 == 0):
