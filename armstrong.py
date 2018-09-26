6adb0dfcfd628b85985bc8d9ef877d189ecc9291
amirakm <42800800+amirakm@users.noreply.github.com>
2018-09-26 10:56:20 +0530

    Create armstrong.py

---
+++ armstrong.py
@@ -0,0 +1,18 @@
+#program to find armstrong number or not
+num = int(input(""))
+
+# initialize sum
+sum = 0
+
+# find the sum of the cube of each digit
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+
+# display the result
+if num == sum:
+   print("yes")
+else:
+   print("no")
